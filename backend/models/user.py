dbd1b10f9a62ee5ef35584a7f974fee524e3b194
Will Tekulve <tekulve.will@gmail.com>
2017-06-20 20:17:28 -0400

    Create user datastore model

---
+++ backend/models/user.py
@@ -0,0 +1,14 @@
+from google.appengine.ext import ndb
+
+# Property Information can be found here:
+# https://cloud.google.com/appengine/docs/standard/python/ndb/entity-property-reference
+
+
+# noinspection PyUnresolvedReferences
+class User(ndb.Model):
+    fname = ndb.StringProperty()
+    lname = ndb.StringProperty()
+    phone_num = ndb.IntegerProperty()
+
+    oauth_id = ndb.StringProperty(required=True)
+    create_date = ndb.DateTimeProperty(auto_no_add=True)
