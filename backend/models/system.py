1f2268aa0b32b90faebb046ffba105dcc983a903
Eric <gattoej@mail.uc.edu>
2017-06-27 18:22:53 -0400

    Add system and system model

--- backend/models/system.py
+++ backend/models/system.py
@@ -6,3 +6,8 @@ class System(ndb.Model):
     alarm_count = ndb.IntegerProperty()
     ks_enabled = ndb.BooleanProperty(required=True)
     create_date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Secondary(ndb.Model):
+    oauth_id = ndb.StringProperty(required=True)
+    system_id = ndb.StringProperty(required=True)
