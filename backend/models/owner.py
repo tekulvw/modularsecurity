3e69fdaaeb04b7cdcbdd4db87a9f5363dbcace3d
Eric <gattoej@mail.uc.edu>
2017-06-27 18:52:31 -0400

    Added resource file for system
    Added resource file for system
    
    Added resource file for system
    
    Added resource file for system

--- backend/models/owner.py
+++ backend/models/owner.py
@@ -1,6 +1,19 @@
 from google.appengine.ext import ndb
 
+from models import system as SystemModel
+from models import user as UserModel
 
 class Owner(ndb.Model):
     user_key = ndb.KeyProperty(kind="User")
     system_key = ndb.KeyProperty(kind="System")
+
+    @classmethod
+    def create(cls,oauth,grace):
+        user_key = UserModel.User.from_oauth_id(oauth)
+        system_key = SystemModel.System.create(grace)
+        return cls(
+            user_key=user_key,
+            system_key=system_key
+        )
+
+
