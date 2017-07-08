359ca816c6eb30e5c4fc2972eb694cd98383ac2f
John D'Alessandro <dalessandro.john.24@gmail.com>
2017-07-08 14:53:23 -0400

    Displays user info on the page itself

--- frontend/app/src/NavController.js
+++ frontend/app/src/NavController.js
@@ -1,19 +1,27 @@
 function NavController($mdSidenav) {
   var self = this;
-  var userInfo;
-  $.get("/api/user", function(data, status){
-    userInfo = data;
-    console.log(userInfo)
+ 
+  $.ajax({
+    url: "/api/user",
+    success: function(data, status){
+        self.userInfo = data;
+      },
+    async: false
   });
+
+  self.getUserName = function(){
+    return self.userInfo.fname.toUpperCase() + " FUCKING " + self.userInfo.lname.toUpperCase();
+  };
+
   self.bodyText = "You're on the monitor screen"
   self.navMonitor = function(){
   	self.bodyText = "You're on the monitor screen"
-  }
+  };
   self.navManagement = function(){
   	self.bodyText = "You're on the management screen"
-  }
+  };
   self.navSettings = function(){
   	self.bodyText = "You're on the account settings screen"
-  }
+  };
 };
 export default [ '$mdSidenav', NavController ];
