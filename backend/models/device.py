970eb66163967058be2f19d9526e833f6bf9524f
Rosejo <tough990@gmail.com>
2017-07-08 13:01:42 -0400

    First shot resources\device.py with big changes done to models\device.py

--- backend/models/device.py
+++ backend/models/device.py
@@ -8,6 +8,15 @@ class Device(ndb.Model):
     is_connected = ndb.BooleanProperty()
     device_type_key = ndb.IntegerProperty()
 
+    def to_json(self):
+        data={
+            "serial_num": self.serial_num,
+            "system_key": self.system_key,
+            "name": self.name,
+            "is_connected": self.is_connected,
+            "device_type_key": self.device_type_key
+        }
+        return data
 
 class DeviceData(ndb.Model):
     location = ndb.StringProperty()
@@ -15,6 +24,36 @@ class DeviceData(ndb.Model):
     device_key = ndb.KeyProperty(kind="Device")
     grace_period = ndb.IntegerProperty()
 
+    def to_json(self):
+        data={
+            "location": self.location,
+            "data_received": self.data_received,
+            "device_key": self.device_key,
+            "grace_period": self.grace_period
+        }
+        return data
+    def from_device_serial_number(cls,serial_number):
+        return cls.query(cls.serial_num == serial_number)
+
+    @classmethod
+    def create(cls, key , data):
+        location = data.get('location')
+        data_received = data.get('data_received')
+        device_key = key
+        grace_period = data.get('grace_period')
+
+        return cls(
+            location=location,
+            data_received=data_received,
+            device_key=device_key,
+            grace_period=grace_period
+        )
+
 
 class DeviceDataType(ndb.Model):
     type_name = ndb.StringProperty()
+
+    def to_json(self):
+        data={
+            "type_name": self.type_name
+        }
---
+++ backend/resources/device.py
@@ -0,0 +1,17 @@
+from flask import current_app as app
+from flask import url_for, redirect, session, jsonify, flash, abort, request
+from flask.views import MethodView
+
+from models import device
+
+class Device(MethodView):
+    def post(self):
+        #serial_number = Device.serial_num()
+        data = request.get_json()
+        if data is None:
+            abort(401)
+        return
+        serial_number = data.get('serial_number')
+        key = from_device_serial_number(serial_number)
+        entry=device.create(key,data)
+        entry.put()
