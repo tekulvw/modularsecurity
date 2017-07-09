6747cf3813780518190ec623900e9b7813907b22
Eric <gattoej@mail.uc.edu>
2017-07-09 14:04:22 -0400

    updated some tests
    update tests
    
    update tests

--- backend/tests/test_models/test_system.py
+++ backend/tests/test_models/test_system.py
@@ -1,7 +1,6 @@
 import pytest
 
 
-
 @pytest.fixture
 def system_cls():
     from models.system import System
@@ -11,3 +10,7 @@ def system_cls():
 def test_system_attrs(system_cls):
     attrs = ("grace_period", "alarm_count", "ks_enabled","create_date")
     assert all(hasattr(system_cls, attr) for attr in attrs) is True
+
+def test_real_json(random_system):
+    import json
+    assert random_system.to_json() == json.loads(json.dumps(random_system.to_json()))
