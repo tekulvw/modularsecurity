7f264e9885683316c98ac447d18e6101735ef11b
Will Tekulve <tekulve.will@gmail.com>
2017-07-12 11:11:36 -0400

    Device updating test

--- backend/tests/test_resources/test_device.py
+++ backend/tests/test_resources/test_device.py
@@ -58,17 +58,36 @@ def test_device_creation(admin_app):
     assert Device.from_serial_number(serial_number) is not None
 
 
+def test_device_updating(logged_in_app, random_device):
+    data = {
+        "name": "somethingnew",
+        "enabled": not random_device.enabled
+    }
+    url = "/api/device/{}".format(random_device.serial_num)
+
+    assert random_device.name != data["name"]
+
+    with logged_in_app:
+        resp = logged_in_app.put(url, data=json.dumps(data),
+                                 headers={'content-type': 'application/json'})
+    assert resp.status_code == 200
+
+    assert random_device.name == "somethingnew"
+    assert random_device.enabled == data["enabled"]
+
+
 def test_system_association(logged_in_app, random_system, random_device_nosystem):
     data = {
         'system_id': random_system.key.integer_id()
     }
     url = "/api/device/{}".format(random_device_nosystem.serial_num)
 
+    assert random_device_nosystem.system_key is None
+
     with logged_in_app:
         resp = logged_in_app.put(url, data=json.dumps(data),
                                  headers={"content-type": "application/json"})
     assert resp.status_code == 200
 
-    new_device = random_device_nosystem.key.get()
-    assert new_device.system_key == random_system.key
+    assert random_device_nosystem.system_key == random_system.key
 
