7d6076757ff68480fdeec71067757b539f7c3960
Will Tekulve <tekulve.will@gmail.com>
2017-07-13 12:54:23 -0400

    Stop users able to see all systems

--- backend/models/secondary.py
+++ backend/models/secondary.py
@@ -4,3 +4,16 @@ from google.appengine.ext import ndb
 class Secondary(ndb.Model):
     system_key = ndb.KeyProperty(kind="System")
     user_key = ndb.KeyProperty(kind="User")
+
+    @classmethod
+    def from_user(cls, user):
+        """
+        Returns a list of Secondaries from a given user.
+        :param user: Datastore object
+        :return: list of datastore objects
+        """
+        q = cls.query(cls.user_key == user.key)
+        count = q.count()
+        if count > 0:
+            return q.fetch(count)
+        return []
