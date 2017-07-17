9cb3780dc90a12996bfddfb19ef3e55207ea8c2e
Will Tekulve <tekulve.will@gmail.com>
2017-07-16 22:49:30 -0400

    Real fix time

--- backend/storage/__init__.py
+++ backend/storage/__init__.py
@@ -5,6 +5,6 @@ __all__ = ["store_data", ]
 
 BUCKET_NAME = os.environ.get("BUCKET_NAME", app_identity.get_default_gcs_bucket_name())
 
-BUCKET_PREFIX = "/" + BUCKET_NAME + "/devicedata/{}"
+BUCKET_PREFIX = "/" + BUCKET_NAME + ".appspot.com/devicedata/{}"
 
 from .writer import store_data
