2f570a54aae4e698c3ac6a4fcf01e07473342a88
John D'Alessandro <dalessandro.john.24@gmail.com>
2017-07-20 13:38:24 -0400

    monitor tab disabled switches if user is a secondary

--- frontend/app/src/MonitorController.js
+++ frontend/app/src/MonitorController.js
@@ -1,15 +1,20 @@
 function MonitorController() {
   var self = this;
 
-  self.toggleEnable = function(ev, device){
-  	$.ajax({
-	  	  url: "/api/device/" + device.serial_num,
-	  	  type: "PUT",
-	  	  success: function(data){},
-	      data: JSON.stringify({'enabled': !device.enabled}),
-	      dataType: 'json',
-	      contentType: 'application/json'
-	  });
+  self.toggleEnable = function(ev, device, permission){
+  	if(permission){
+	  	$.ajax({
+		  	  url: "/api/device/" + device.serial_num,
+		  	  type: "PUT",
+		  	  success: function(data){},
+		      data: JSON.stringify({'enabled': !device.enabled}),
+		      dataType: 'json',
+		      contentType: 'application/json'
+		  });
+  	}
+  	else{
+  		//toasty
+  	}
   }
 };
 export default [ MonitorController ];
