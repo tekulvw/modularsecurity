f8a035db01cc15da7fa2421e7ab1ddf4a5c70468
John D'Alessandro <dalessandro.john.24@gmail.com>
2017-07-20 13:17:53 -0400

    enable/disable devices on the monitor tab

--- frontend/app/src/MonitorController.js
+++ frontend/app/src/MonitorController.js
@@ -1,4 +1,15 @@
 function MonitorController() {
   var self = this;
+
+  self.toggleEnable = function(ev, device){
+  	$.ajax({
+	  	  url: "/api/device/" + device.serial_num,
+	  	  type: "PUT",
+	  	  success: function(data){},
+	      data: JSON.stringify({'enabled': !device.enabled}),
+	      dataType: 'json',
+	      contentType: 'application/json'
+	  });
+  }
 };
 export default [ MonitorController ];
